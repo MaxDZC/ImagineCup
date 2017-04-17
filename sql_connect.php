42be9ef60a1f01fe19594bba100e02dab90f7c6b
Max Zuorba <maxtimedout@gmail.com>
2017-04-17 09:23:03 +0800

    Almost there!

--- sql_connect.php
+++ sql_connect.php
@@ -3,7 +3,7 @@ $mysqli = new mysqli("localhost", "root", "", "oneschooldb");
 
 if($mysqli->connect_error){
 	$mysqli = new mysqli("ap-cdbr-azure-southeast-b.cloudapp.net", "ba6c086e8940fd", "a5c3978a", "oneschool");
-	if($mysqli) {
+	if(!$mysqli) {
 		header("location: db_error.php");
 	}
 }
